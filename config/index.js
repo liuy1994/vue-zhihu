3fd62b163ac16240f34588012645b2cb38a3c01f
liuy1994 <liuy1994@outlook.com>
2017-12-08 22:03:59 +0800

    finish topbar

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api':{
-        target:'http://news-at.zhihu.com',
+        target:'//news-at.zhihu.com',
         changeOrigin: true,
         pathRewrite:{
           '^/api':'/api'
--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="topbar">
-      <img src="" alt="logo">
+      <img src="../assets/img/top_logo.png" alt="logo">
       <div class="buttons">
-          <button>App 下载</button>
-          <button>浏览内容</button>
+          <a href="/123"><span class="app">App 下载</span></a>
+          <a href="/321"><span>浏览内容</span></a>
       </div>
   </div>
 </template>
@@ -14,9 +14,11 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
   .topbar{
-    border:1px solid black;
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
     position: fixed;
     top:0;
     left: 0;
@@ -24,5 +26,27 @@ export default {
     height: 78px;
     width:960px;
     margin:0 auto;
+    img{
+      height: 60px;
+    }
+    .buttons{
+      display: flex;
+      a{
+        display: block;
+        width:140px;
+        line-height: 78px;
+        text-align: center;
+        span{
+          padding: 10px;
+        }
+        span.app{
+          background: #f0f9ff;
+          color: #008bed;
+          border-radius: 3px;
+        }
+      }
+    }
+    
+
   }
 </style>
