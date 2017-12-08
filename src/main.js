3b503e8205d175050e17fc52c2738ceafa3f8f02
liuy1994 <liuy1994@outlook.com>
2017-12-08 22:41:05 +0800

    write ajax.js > main.js

--- src/main.js
+++ src/main.js
@@ -13,3 +13,19 @@ new Vue({
   template: '<App/>',
   components: { App }
 })
+
+var ajax = new XMLHttpRequest
+
+ajax.onreadystatechange = function(res){
+    if(ajax.readyState === 4 && ajax.status === 200){
+        console.log(JSON.parse(ajax.responseText))
+        console.log('成功')
+    }
+}
+
+ajax.open('GET','/api/4/news/latest')
+
+ajax.send()
+
+
+console.log(123)
