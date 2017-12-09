fb17319c3ea511189186b6dab49484582f67e9dd
liuy1994 <liuy1994@outlook.com>
2017-12-09 23:21:54 +0800

    lleony.pro > liuy1994@github.io/vue-zhihu

--- config/index.js
+++ config/index.js
@@ -63,7 +63,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-zhihu/dist',
+    assetsPublicPath: '/dist',
 
     /**
      * Source Maps
